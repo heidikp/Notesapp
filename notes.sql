ce37115b3fd6afa58862e3acf62fa10517823b67
heidikp <heidi.peltola@student.laurea.fi>
2017-05-25 14:28:01 +0300

    Better version of SQL and Readme
    In this one there is also a way to add a "test" patient.

--- notes.sql
+++ notes.sql
@@ -1,11 +1,19 @@
 CREATE DATABASE notes;
 
-CREATE TABLE patients (
-Name varchar(30),
+CREATE TABLE patient (
+Name varchar(30) ,
 Place varchar(2),
 Weeks varchar(4),
 Para varchar(10),
-Allergies varchar(255),
-Info varchar(255),
-Notes varchar(255)
+Allergies varchar(30,
+Info varchar(500),
+Notes varchar(500)
+PRIMARY KEY (Name)
 );
+
+-Insert a patient to test the program
+
+INSERT INTO patient
+(Name, Place, Weeks, Para, Info, Notes)
+VALUES
+('Maija Malli', '1', '24+3', 'G4P2', 'ASA', 'hptg', 'rrx3');  
